5782237d95c7e8c9f893d60acfc162554567c8dc
Renan <renanjesus4321@gmail.com>
2023-04-12 00:45:50 -0300

    primeiro commit

---
+++ TrabalhoPoo1Bi/src/main/java/com/mycompany/trabalhopoo1bi/TrabalhoPoo1Bi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhopoo1bi;
+
+/**
+ *
+ * @author renan
+ */
+public class TrabalhoPoo1Bi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
